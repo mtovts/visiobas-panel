f926b28e70b07a98ff7040f1f21cd9f891522cc7
mtovts <lllghtz@outlook.com>
2021-03-15 14:37:31 +0300

    set tsl

--- mqtt.py
+++ mqtt.py
@@ -47,6 +47,7 @@ class VisioMQTTClient:  # (Thread):
             # transport='tcp',
             transport='websockets'
         )
+        self._client.tls_set()
         # self._client.enable_logger()  # logger=logger
         self._client.username_pw_set(username=self._username, password=self._password)
 
