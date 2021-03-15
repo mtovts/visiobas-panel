ac9c65ada03d91a11b5f7abd5cf9a5c43e6c4d19
mtovts <lllghtz@outlook.com>
2021-03-15 14:39:25 +0300

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -44,10 +44,10 @@ class VisioMQTTClient:  # (Thread):
         self._connected = False
 
         self._client = mqtt.Client(
-            # transport='tcp',
-            transport='websockets'
+            transport='tcp',
+            # transport='websockets'
         )
-        self._client.tls_set()
+        # self._client.tls_set()
         # self._client.enable_logger()  # logger=logger
         self._client.username_pw_set(username=self._username, password=self._password)
 
